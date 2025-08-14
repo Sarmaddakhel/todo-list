1d4a45f177f71261571b8d1ad63cc502bec55827
sarmaddakhel <sdm997888@gmail.com>
2025-08-14 21:16:39 +0300

    Deployment commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,38 @@
+// webpack.config.js
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-  mode: "production", // change to 'production' for hosting
+  mode: "development",
   entry: "./src/index.js",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
-    publicPath: "/todo-list/", // matches your GitHub Pages repo
     clean: true,
   },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader, "css-loader"], // extract CSS
-      },
-      {
-        test: /\.html$/i,
-        loader: "html-loader", // handles template.html imports
-      },
-      {
-        test: /\.(png|jpg|jpeg|gif|svg)$/i,
-        type: "asset/resource", // handle images if any
-      },
-    ],
+  devtool: "eval-source-map",
+  devServer: {
+    watchFiles: ["./src/template.html"],
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/template.html",
-      filename: "index.html",
-    }),
-    new MiniCssExtractPlugin({
-      filename: "style.css",
     }),
   ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
 };
